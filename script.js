7225e46a5ca21be787c626dd769abb082f5e1e28
Abdlrhman <abdlrhmanv@icloud.com>
2024-12-17 16:16:03 +0200

    Add a to do list

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document,addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
